bce4d3f4dd69a096bb1b7613703006a3a06b8781
Franky Cunza <franky.cunza@prix.tips>
2020-05-18 18:34:23 -0500

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
 })
-export class AppComponent {
+export class AppComponent { 
   title = 'joyeriasafn';
+
+
+  ngOnInit(): void {
+
+  }
+
+
+
 }
